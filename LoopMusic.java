4bf12611b14b5d9b2b6b3a0e52c58777da944028
KevinCompany-CodingOfficial <30421671+KevinCompany-CodingOfficial@users.noreply.github.com>
2021-08-23 10:52:56 -0700

    Update LoopMusic.java

--- LoopMusic.java
+++ LoopMusic.java
@@ -3,7 +3,7 @@ public class LoopMusic {
 
     public static void main(String[] args) {
 
-        String filepath = "C:\\Users\\adazh\\OneDrive\\Desktop\\Music\\studymusic_1_yellowbrick.mp3";
+        String filepath = "C:\\Users\\kevin\\OneDrive\\Desktop\\Music\\studymusic_1_yellowbrick.mp3";
 
         MusicPlayer musicObject = new MusicPlayer();
         musicObject.playMusic(filepath);
